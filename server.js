12e0e670ccb7fa84b1ee76807b4b90e922138393
JYassine <yassine.jrad@etu.unice.fr>
2021-03-25 15:24:00 +0100

    procfile

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 import express from "express";
 import routes from "./routes.js";
-
-import dotenv from "dotenv";
-const dotenvConfig = dotenv.config();
 import cors from "cors";
-
 import bodyParser from "body-parser";
 
-if (dotenvConfig.error) {
-  throw dotenvConfig.error;
-}
 const app = express();
 const port = process.env.PORT || 3000;
 
